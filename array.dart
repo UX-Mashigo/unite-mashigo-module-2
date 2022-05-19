ca75ed8364590422e2d05f5296e9cf3b306cc657
Unite Mashigo <unite.mashigo@dadevs.co.za>
2022-05-19 15:58:48 +0200

    list commit

---
+++ array.dart
@@ -0,0 +1,17 @@
+// Ambani Africa- Mukundi Lambani 2021
+// EasyEquities 2020
+// Naked insure 2019- insureteck startup
+// Khula ecosystem 2018- karidas Tshintsholo
+// Shyft 2017
+// Domestly 2016 -cleaning-on-demand startup
+// Gumdrop 2015 Simon hurtle
+// LIVE Inspect- 2014
+// SnapScan 2013
+// FNB banking app 2012
+
+void main(){
+  var listOfApps=["Ambani Africa", "EasyEquities"," Naked insure","Khula ecosystem","Shyft","Domestly", "Gumdrop","LIVE Inspect","SnapScan","FNB banking app" ];
+  print("List of Apps $listOfApps");
+}
+
+   
---
+++ profile.dart
@@ -0,0 +1,10 @@
+void main()
+{
+   String name = "Unite ";
+   var app= "Xitsonga Dictionary";
+   String city = "Johannesburg";
+   var listOfApps=["Bus App", "Xitsonga Dictionary, doctor App"]
+   print("Hi, my name is $name + , my favorite app is $app + and I live in $city");
+   print(List of Apps $listOfApps);
+}
+
